19c4add5d8adbe05b85153164b69cd7fee6cddf4
Blake Creasser <creasser@Creasser.>
2023-08-29 18:39:27 -0500

    add category table, setup db relationships for it

--- app/models/trick.rb
+++ app/models/trick.rb
@@ -1,6 +1,7 @@
 class Trick < ApplicationRecord
     has_many :goals
     has_many :users, through: :goals
+    belongs_to :category
     validates :title, uniqueness: true, 
     validates :difficulty, greater_than_or_equal_to: 0, less_than_or_equal_to: 5
 end

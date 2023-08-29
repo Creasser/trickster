e0730450b79cad50ed0770b65270e33bb4b5ab4d
Blake Creasser <creasser@Creasser.>
2023-08-29 18:49:29 -0500

    start working on user signup route in the backend

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  post "/usersignup", to: "user#create"
+    
 end

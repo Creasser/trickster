0274518cf419290569a4892dbbcac5046abead11
Blake Creasser <creasser@Creasser.>
2023-08-29 18:26:23 -0500

    setup db relationship for user and goals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :goals
     has_secure_password
     validates :username, uniqueness: true, presence: true, length: {8..20}
     validates :email, uniqueness: true, presence: true

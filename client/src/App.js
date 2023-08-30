bf78e159c4b7fd47c65bd2ed86bd92156c07f935
Blake Creasser <creasser@Creasser.>
2023-08-29 19:03:13 -0500

    clean up client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0);

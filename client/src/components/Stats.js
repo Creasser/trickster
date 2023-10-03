75eaacfaf53225dd5bf4d5ac591c3bf7e18c900f
Blake Creasser <creasser@Creasser.>
2023-10-02 19:49:44 -0500

    highest attempts and completed tricks now shown in stats

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -18,14 +18,24 @@ function Stats(){
        return goals.length
     }
 
+    function highestAttemptedTrick(){
+        const goals = user.goals.sort((a, b) => b.attempts - a.attempts)
+        const trick = goals[0].trick
+        return trick.title
+    }
+
     return(
         <div>
             <h1>Stats Page</h1>
             <div>
                 <h3>Tricks Completed</h3>
                 <h4>{completedTricks()}</h4>
-                <button onClick={completedTricks}>Log Function</button>
             </div>
+            <div>
+                <h3>Most Attempted Trick</h3>
+                <h4>{highestAttemptedTrick()}</h4>
+            </div>
+                <button onClick={highestAttemptedTrick}>Log Function</button>
         </div>
     )
 }

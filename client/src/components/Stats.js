8ffd673c4c68d89476d2c21c5fe5c7f5059c57fe
Blake Creasser <creasser@Creasser.>
2023-10-02 20:09:51 -0500

    avg difficulty now shown

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -24,6 +24,25 @@ function Stats(){
         return trick.title
     }
 
+    function leastAttemptedTrick(){
+        const goals = user.goals.sort((a, b) => a.attempts - b.attempts)
+        const trick = goals[0].trick
+        return trick.title
+    }
+
+    function averageDifficulty(){
+        const completedTricks = user.goals.filter((goal) => {
+            return goal['is_completed'] === true
+        })
+        const goalDifficulty = completedTricks.map((goal) => {
+            return goal.trick['difficulty']
+        })
+        const difficultyTotal = goalDifficulty.reduce((a, b) => a + b, 0)
+        console.log(difficultyTotal / completedTricks.length)
+        console.log(difficultyTotal)
+        return difficultyTotal / completedTricks.length
+    }
+
     return(
         <div>
             <h1>Stats Page</h1>
@@ -35,7 +54,15 @@ function Stats(){
                 <h3>Most Attempted Trick</h3>
                 <h4>{highestAttemptedTrick()}</h4>
             </div>
-                <button onClick={highestAttemptedTrick}>Log Function</button>
+            <div>
+                <h3>Least Attempted Trick</h3>
+                <h4>{leastAttemptedTrick()}</h4>
+            </div>
+            <div>
+                <h3>Average Difficulty</h3>
+                <h4>{averageDifficulty()}</h4>
+            </div>
+                <button onClick={averageDifficulty}>Log Function</button>
         </div>
     )
 }

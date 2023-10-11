c3a7525382f9dbae5c27ac83c5fafb7ea2593e0e
Blake Creasser <creasser@Creasser.>
2023-10-10 20:03:57 -0500

    finish basic style on signup page

--- client/src/components/Singup.js
+++ client/src/components/Singup.js
@@ -3,7 +3,7 @@ import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Error from "./Error";
 import { UserContext } from "./Context";
 import { v4 as uuidv4 } from "uuid";
-import { TextField, Button, Stack, Grid } from '@mui/material'
+import { TextField, Button, Stack, Grid, Divider } from '@mui/material'
 
 function Signup(){
     const history = useHistory()
@@ -53,53 +53,62 @@ function Signup(){
     }
 
     return(
-        <div>
-            <h1>Sign Up Here</h1>
-            <div>
-                <form autoComplete="off" onSubmit={handleSubmit}>
-                    <Stack spacing={2} sx={{width: .5}}>
-                        <TextField
-                            type="text"
-                            name="username"
-                            value={userSignup.username}
-                            placeholder="Enter Username"
-                            onChange={handleChange}/>
-                        <TextField
-                            type="text"
-                            name="password"
-                            value={userSignup.password}
-                            placeholder="Enter Password"
-                            onChange={handleChange}/>
-                        <TextField
-                            type="text"
-                            name="password_confirmation"
-                            value={userSignup.password_confirmation}
-                            placeholder="Confirm Password"
-                            onChange={handleChange}/>
-                        <TextField
-                            type="text"
-                            name="email"
-                            value={userSignup.email}
-                            placeholder="Enter Email"
-                            onChange={handleChange}/>
-                        <Button 
-                            type="submit"
-                            name="submit"
-                            variant="outlined"
-                            >Sign Up
-                        </Button>
-                    </Stack>
-                </form>
-                <div>
-                {errors ? 
-                    errors.map((err) => {
-                        return <Error key={uuidv4()} err={err} />
-                    })
-                    :
-                    null}
-            </div>
-            </div>
-            <Link to="/">Return</Link>
+        <div style={{height: "100vh"}}>
+            <Grid container alignItems='stretch' justifyContent='center'>
+                <Grid item xs={6}>
+                    <div style={{height: '100vh'}}>
+                        <img src="https://img.cdn-pictorem.com/uploads/collection/O/OA8RCH8DHG/900_1891766HighRes.jpg" style={{height:'100%', width:'100%', objectFit:'cover'}}></img>
+                    </div>
+                </Grid>
+                <Grid item xs={6} >
+                    <div style={{height: '100vh', display:'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center'}}>
+                        <h1>Sign Up Here</h1>
+                            <form onSubmit={handleSubmit} style={{width: '70%', display:'flex', justifyContent:'center'}}>
+                                <Stack spacing={2} sx={{width: .75}} divider={<Divider orientation="horizontal" flexItem />}>
+                                    <TextField
+                                        type="text"
+                                        name="username"
+                                        value={userSignup.username}
+                                        placeholder="Enter Username"
+                                        onChange={handleChange}/>
+                                    <TextField
+                                        type="text"
+                                        name="password"
+                                        value={userSignup.password}
+                                        placeholder="Enter Password"
+                                        onChange={handleChange}/>
+                                    <TextField
+                                        type="text"
+                                        name="password_confirmation"
+                                        value={userSignup.password_confirmation}
+                                        placeholder="Confirm Password"
+                                        onChange={handleChange}/>
+                                    <TextField
+                                        type="text"
+                                        name="email"
+                                        value={userSignup.email}
+                                        placeholder="Enter Email"
+                                        onChange={handleChange}/>
+                                    <Button 
+                                        type="submit"
+                                        name="submit"
+                                        variant="outlined"
+                                        >Sign Up
+                                    </Button>
+                                </Stack>
+                            </form>
+                            <small>Already have an account? <Link to='/login'>Login Here</Link></small>
+                        <div>
+                        {errors ? 
+                            errors.map((err) => {
+                                return <Error key={uuidv4()} err={err} />
+                            })
+                            :
+                            null}
+                    </div>
+                    </div>
+                </Grid>
+        </Grid>
         </div>
     )
 }
